12473d40d71fe597d339b679de33638190454176
amartahir895-commits <amartahir895@gmail.com>
2026-01-19 10:37:43 +0500

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,15 @@
+module.exports = {
+  nameBot: "AMMAR H4CK3R",
+  prefix: [".", "!", "/"],
+
+  // Daftar Owner Default (pakai nomor tanpa @s.whatsapp.net)
+  owner: [
+    "923195447147",  // Owner utama
+  ],
+ savepushkontak: "./database/pushkontak/",
+ pushkontak: "pushkontak",
+
+  // Link & thumbnail (opsional)
+  lynk: "https://github.com/famofc",
+  thumb3: "https://telegra.ph/file/xxx.jpg",
+};
